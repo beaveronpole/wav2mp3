227f111963c459e2af382061b996c7dc78176e21
beaver <beaver@beaveronpole.com>
2019-12-31 14:07:18 +0300

    add empty class for multithreading encoding.

---
+++ src/WAVFilesConverter.cpp
@@ -0,0 +1,9 @@
+//
+// Created by beaver on 12/31/19.
+//
+
+#include "WAVFilesConverter.h"
+
+WAVFilesConverter::WAVFilesConverter() {
+
+}
---
+++ src/WAVFilesConverter.h
@@ -0,0 +1,26 @@
+//
+// Created by beaver on 12/31/19.
+//
+
+#ifndef WAV2MP3_WAVFILESCONVERTER_H
+#define WAV2MP3_WAVFILESCONVERTER_H
+
+#include <pthread.h>
+#include <iostream>
+#include <list>
+#include <string>
+
+#include "WAVFileConverter.h"
+
+/*
+ * Class makes several workers to encode file from given list
+ */
+
+class WAVFilesConverter {
+public:
+    WAVFilesConverter();
+
+};
+
+
+#endif //WAV2MP3_WAVFILESCONVERTER_H
